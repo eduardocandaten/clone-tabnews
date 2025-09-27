15d3850ce672eac31bd476501f70a2f7628bdbb0
Eduardo Müller Candaten <149803596+eduardocandaten@users.noreply.github.com>
2025-09-27 09:24:07 -0300

    add `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,10 @@
-function status(request, response) {
-  response.status(200).json({ user: "teçá ." });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result);
+
+  response.status(200).json({ user: "eduardo" });
 }
 
 export default status;
