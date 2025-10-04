2a3039c5831ab8b0e64a42e9272f9c288dec7122
Eduardo Müller Candaten <149803596+eduardocandaten@users.noreply.github.com>
2025-10-04 10:05:36 -0300

    add ssl value dynamic

--- infra/database.js
+++ infra/database.js
@@ -7,18 +7,29 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
 
-  await client.connect();
-
   try {
+    await client.connect();
+
     const result = await client.query(queryObject);
+
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return { ca: process.env.POSTGRES_CA };
+  }
+
+  return process.env.NODE_ENV !== "development";
 }
 
 export default { query: query };
