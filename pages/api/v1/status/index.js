b00c0a42c625e229d5dcdf58e069a141fda1b276
Eduardo Müller Candaten <149803596+eduardocandaten@users.noreply.github.com>
2025-10-22 14:22:36 -0300

    fix: rename `update_at` to `updated_at` in endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const updateAt = new Date().toISOString();
+  const updatedAt = new Date().toISOString();
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
   const databaseMaxConnectionsResult = await database.query(
@@ -19,7 +19,7 @@ async function status(request, response) {
     databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
-    update_at: updateAt,
+    updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,11 +9,11 @@ describe("GET api/v1/status", () => {
     test("Retrieving current system status", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status");
       const responseBody = await response.json();
-      const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
       expect(response.status).toBe(200);
-      expect(responseBody.update_at).toBeDefined();
-      expect(responseBody.update_at).toEqual(parsedUpdatedAt);
+      expect(responseBody.updated_at).toBeDefined();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
       expect(responseBody.dependencies.database.version).toEqual("16.0");
       expect(responseBody.dependencies.database.max_connections).toEqual(100);
       expect(responseBody.dependencies.database.opened_connections).toEqual(1);
