3795acf9254d88e8e3851cbb578693a49ae45f62
Eduardo Müller Candaten <149803596+eduardocandaten@users.noreply.github.com>
2025-10-24 17:30:31 -0300

    feat: use `ServiceError` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -9,9 +10,11 @@ async function query(queryObject) {
 
     return result;
   } catch (error) {
-    console.log("\nErro dentro do catch do database");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com o Banco ou na Query",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
