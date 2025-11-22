6f2a030f02503c9b8e5fc18f8e125f7d76ad8009
Eduardo Müller Candaten <149803596+eduardocandaten@users.noreply.github.com>
2025-11-22 16:30:20 -0300

    feat: create `password` model and use hash method in `user.create`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,7 @@
 import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator.js";
+import user from "models/user.js";
+import password from "models/password.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -22,17 +24,28 @@ describe("POST api/v1/users", () => {
         }),
       });
       const responseBody = await response.json();
+      const userInDatabase = await user.findOneByUsername("username");
+      const correctPasswordMatch = await password.compare(
+        "password123",
+        userInDatabase.password,
+      );
+      const incorrectPasswordMatch = await password.compare(
+        "incorrectpassword",
+        userInDatabase.password,
+      );
 
       expect(response.status).toBe(201);
       expect(responseBody).toEqual({
         id: responseBody.id,
         username: "username",
         email: "exemple@email.com",
-        password: "password123",
+        password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
       expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(correctPasswordMatch).toBe(true);
+      expect(incorrectPasswordMatch).toBe(false);
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
     });
