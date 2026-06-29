a4b347594aa56581c3f33349411190836b8571af
Eduardo Müller Candaten <149803596+eduardocandaten@users.noreply.github.com>
2026-06-29 12:57:22 -0300

    feat: add `features` column to `users` table

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -39,6 +39,7 @@ describe("POST api/v1/users", () => {
         id: responseBody.id,
         username: "username",
         email: "exemple@email.com",
+        features: [],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
