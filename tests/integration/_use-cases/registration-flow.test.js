3d10d65d7f3ec9a2a68067324b998f2d30b1969f
Eduardo Müller Candaten <149803596+eduardocandaten@users.noreply.github.com>
2026-06-29 13:22:55 -0300

    feat: add default feature  when creating

---
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -0,0 +1,39 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+  await orchestrator.deleteAllEmails();
+});
+
+describe("Use case: Registration Flow (All successful)", () => {
+  test("Create user account", async () => {
+    const createUserResponse = await fetch(
+      "http://localhost:3000/api/v1/users",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "RegistrationFlow",
+          email: "registration.flow@email.com",
+          password: "password123",
+        }),
+      },
+    );
+    const createUserResponseBody = await createUserResponse.json();
+
+    expect(createUserResponse.status).toBe(201);
+    expect(createUserResponseBody).toEqual({
+      id: createUserResponseBody.id,
+      username: "RegistrationFlow",
+      email: "registration.flow@email.com",
+      features: ["read:activation_token"],
+      password: createUserResponseBody.password,
+      created_at: createUserResponseBody.created_at,
+      updated_at: createUserResponseBody.updated_at,
+    });
+  });
+});
